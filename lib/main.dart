50ab030cc5a8baaf0107b84a6b726dd7d1461d2c
Jag Nanda <jnanda313@gmail.com>
2020-10-11 11:35:02 -0400

    updated file names based on dart naming conventions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tutoring_app_flutter/pages/tutor/TutorDashboard.dart';
+import 'package:tutoring_app_flutter/pages/tutor/tutor_dashboard.dart';
 import 'pages/login_page.dart';
 
 void main() {

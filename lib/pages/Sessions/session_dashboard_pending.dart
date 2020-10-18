20174cda3d1ab7fe20b755bc0e8a3909268a6328
BrandonSarmany <45214313+BrandonSarmany@users.noreply.github.com>
2020-10-18 03:48:43 -0400

    Added Session pages. Redirecting with bottom nav bar is a bit weird as our appbar also changes throughout the pages. Working almost as intended through sessions_dashboard.dart

--- lib/pages/Sessions/session_dashboard_pending.dart
+++ lib/pages/Sessions/session_dashboard_pending.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tutoring_app_flutter/components/sessions/pending_sessions.dart';
 
 class SessionDashboardPending extends StatefulWidget {
   @override
@@ -6,8 +7,55 @@ class SessionDashboardPending extends StatefulWidget {
 }
 
 class _SessionDashboardPendingState extends State<SessionDashboardPending> {
+  List<PendingSession> myList = List.generate(10, (index) => PendingSession());
+  ScrollController _scrollController = ScrollController();
+  int shownPostCount = 10;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        fetchMorePosts();
+        setState(() {});
+      }
+    });
+  }
+
+  void fetchMorePosts() {
+    for (int i = shownPostCount; i < shownPostCount + 10; i++) {
+      myList.add(PendingSession());
+    }
+    shownPostCount += 10;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Pending Sessions");
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(20),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: "Search your Sessions",
+              icon: Icon(Icons.search),
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+            ),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            controller: _scrollController,
+            itemCount: myList.length + 1,
+            itemBuilder: (context, index) {
+              if (index == myList.length) {
+                return Center(child: CircularProgressIndicator());
+              }
+              return myList[index];
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
